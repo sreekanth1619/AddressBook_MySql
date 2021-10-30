8fd04214216d0774be2bb3ceb0539d471720f0c7
sreekanth1619 <87491860+sreekanth1619@users.noreply.github.com>
2021-10-30 15:59:29 +0530

    Added: Ability to create address book table

--- AddressBook_MySql.sql
+++ AddressBook_MySql.sql
@@ -21,3 +21,32 @@ mysql> show databases;
 
 mysql> use AddressBookService;
 Database changed
+
+-- UC2  creating table as AddressBook with attributes
+
+
+mysql> create table AddressBook (
+    -> firstName varchar(50) not null,
+    -> lastName varchar(50) not null,
+    -> address varchar(100) not null,
+    -> city varchar(50) not null,
+    -> state varchar(50) not null,
+    -> zip int not null,
+    -> phoneNumber double not null,
+    -> email varchar(100) not null );
+Query OK, 0 rows affected (1.14 sec)
+
+mysql> desc AddressBook;
++-------------+--------------+------+-----+---------+-------+
+| Field       | Type         | Null | Key | Default | Extra |
++-------------+--------------+------+-----+---------+-------+
+| firstName   | varchar(50)  | NO   |     | NULL    |       |
+| lastName    | varchar(50)  | NO   |     | NULL    |       |
+| address     | varchar(100) | NO   |     | NULL    |       |
+| city        | varchar(50)  | NO   |     | NULL    |       |
+| state       | varchar(50)  | NO   |     | NULL    |       |
+| zip         | int          | NO   |     | NULL    |       |
+| phoneNumber | double       | NO   |     | NULL    |       |
+| email       | varchar(100) | NO   |     | NULL    |       |
++-------------+--------------+------+-----+---------+-------+
+8 rows in set (0.13 sec)
