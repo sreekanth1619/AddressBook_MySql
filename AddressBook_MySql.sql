0a3f0cad51a27981f00cecfc0c9f25ec237d22c8
sreekanth1619 <87491860+sreekanth1619@users.noreply.github.com>
2021-10-30 15:52:46 +0530

    Added: Ability to create Address Book database

---
+++ AddressBook_MySql.sql
@@ -0,0 +1,23 @@
+-- AddressBook Service
+
+-- UC1 Creating database as AddessBookService
+
+
+mysql> create database AddressBookService;
+Query OK, 1 row affected (0.13 sec)
+
+mysql> show databases;
++--------------------+
+| Database           |
++--------------------+
+| addressbookservice |
+| information_schema |
+| mysql              |
+| payroll_services   |
+| performance_schema |
+| sys                |
++--------------------+
+6 rows in set (0.00 sec)
+
+mysql> use AddressBookService;
+Database changed
